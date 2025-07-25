843b3135967f23f0f399e313c0be0dc41b8bd1ad
Viktor-Vangelov <viktor.vangelov@gdcgroup.com>
2025-07-25 10:26:14 +0200

    Update seo_anomaly_dashboard.py

--- seo_anomaly_dashboard.py
+++ seo_anomaly_dashboard.py
@@ -1,3 +1,3 @@
-git add seo_anomaly_dashboard.py
-git commit -m "Add Streamlit dashboard version"
-git push origin main
+seo_anomaly_dashboard.py
+-m "Add Streamlit dashboard version"
+push origin main
